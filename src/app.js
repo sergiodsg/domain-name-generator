b79dc86754a5c9796716081420eeb353cbd2d340
sergiodsg <sergiodasilva.dev@gmail.com>
2022-09-13 16:00:54 +0000

    Solución preliminar

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon", "five"];
+let extensions = ["co", "com", "ve", "pt", "es", "org"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let pronounUsed of pronoun) {
+    for (let adjUsed of adj) {
+      for (let nounUsed of noun) {
+        for (let extensionUsed of extensions) {
+          if (nounUsed.endsWith(extensionUsed)) {
+            console.log(
+              pronounUsed +
+                adjUsed +
+                nounUsed.slice(0, -1 * extensionUsed.length) +
+                "." +
+                extensionUsed
+            );
+          } else {
+            console.log(pronounUsed + adjUsed + nounUsed + "." + extensionUsed);
+          }
+        }
+      }
+    }
+  }
 };
